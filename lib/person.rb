3c53d913fd42aba2c4ca373fd52f79d897e0ae67
Blen Wondimagegn <mekuriablen@gmail.com>
2020-09-03 08:43:46 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,7 +4,7 @@ class Person
 :weight, :handed, :complexion, :t_shirt_size, 
 :wrist_size, :glove_size, :pant_length, :pant_width
 
-def initilize(:name, :birthday, :hair_color, :eye_color, :height, 
+def initilize(name:, birthday:, :hair_color:, :eye_color, :height, 
 :weight, :handed, :complexion, :t_shirt_size, 
 :wrist_size, :glove_size, :pant_length, :pant_width)
 
